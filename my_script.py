3782003562bf309b710cce4cb9471b292cfb508e
YASH-TRONX <148982717+YASH-TRONX@users.noreply.github.com>
2023-12-18 20:01:47 +0530

    initial commit

--- my_script.py
+++ my_script.py
@@ -1,1 +1,8 @@
-*/15 * * * * wget https://exptrackdata.onrender.com
+# script.py
+import requests
+
+url = "https://exptrackdata.onrender.com"
+response = requests.get(url)
+
+# You can print or process the response as needed
+print(response.text)
